bcfc1f25f115b2edf3f0fc18345f3a6189c8a6ee
Mustafa Demir <demirmustafa@consultant.turkcell.com.tr>
2022-01-31 20:03:42 +0300

    Initial commit

---
+++ src/main/java/com/project/wop/WopApplication.java
@@ -0,0 +1,13 @@
+package com.project.wop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/wop/WopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
